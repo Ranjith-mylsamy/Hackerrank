8535566c1c5845b6ef44145e67c7b8d923a40ba7
Ranjith <ranjith.19it@kct.ac.in>
2022-02-05 22:57:26 +0530

    sum of digits solution

--- C/sum of the digits/sumofdigits.c
+++ C/sum of the digits/sumofdigits.c
@@ -8,5 +8,12 @@ int main() {
     int n;
     scanf("%d", &n);
     //Complete the code to calculate the sum of the five digits on n.
+        int sum=0;
+    while(n!=0)
+    {
+        sum=sum+n%10;
+        n=n/10;
+    }
+    printf("%d",sum);
     return 0;
 }
